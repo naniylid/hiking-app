67704be04705e9af27893adaf093fa7936cda729
Nadezhda Ilina <nadiailina9@gmail.com>
2024-01-31 14:39:24 +0300

    правки css для навигации и первого листа

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,14 @@ import MainButton from '../Button/Button';
 
 const Header: React.FC = () => {
   return (
-    <div className={styles.root}>
+    <header>
       <img src={image} alt="People walk" />
       <div className={styles.left}>
         <h1>Your path, our guide: Explore the world with our hiking app!</h1>
         <p>Plan your getaway with us </p>
         <MainButton />
       </div>
-    </div>
+    </header>
   );
 };
 
